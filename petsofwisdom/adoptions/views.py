d89c6d9408a048753009014c7e2a63cd35bc51ca
Kayokwa <kayokwa.chibuye@gmail.com>
2020-11-22 18:38:49 -0800

    Create URLs and update views

--- petsofwisdom/adoptions/views.py
+++ petsofwisdom/adoptions/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import Pets
 
 # Create your views here.
+def home(request):
+    pets = Pets.objects.all()
+    return render(request, 'home.html', 
+    {
+        'pets': pets,
+    })
+
+def pet_detail(request, pet_id):
+    try:
+        pet = Pets.objects.get(id=pet_id)
+    except Pets.DoesNotExist:
+        raise Http404 ('Pet not found')
+    return render(request, 'pet_detail.html', 
+    {
+        'pet': pet,
+    })
