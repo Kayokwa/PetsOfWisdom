04f026ed8305220f1e412585b31e43a642ebb865
Kayokwa <kayokwa.chibuye@gmail.com>
2020-11-22 18:01:17 -0800

    Added Admin interface with specific display labels

--- petsofwisdom/adoptions/admin.py
+++ petsofwisdom/adoptions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Pets
+
+@admin.register(Pets)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ['name','species','breed','age','sex']
